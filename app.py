6c4f9fa4e7afd6318c9eb98567b3d3420d54d2ba
Nils Paulson <nille.paulson@gmail.com>
2023-01-02 14:04:25 +0100

    more changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, upgrade
 
-from model import db
+from model import db, seedData, Customer
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Imperialguard1%40@localhost/players0101'
@@ -10,20 +10,33 @@ db.app = app
 db.init_app(app)
 migrate = Migrate(app,db)
 
+@app.route("/")
+def startpage():
+    return "<h1>test<h1>"
+
+@app.route("/kontakt")
+def contactpage():
+    s = "<html><head><title>Get lost</title></head><body></body></html>"
+    for c in Customer.query.all():
+        s = s + c.Name + "<br/>"
+    s = s + "</body></html>"
+    return s
+    
+
 if __name__  == "__main__":
     with app.app_context():
         upgrade()
     
-        #seedData(db)
+        seedData(db)
         app.run()
-        while True:
-            print("1. Create")
-            print("2. List")        
-            print("3. Exit")                
-            action = input("Ange:")
-            if action == "3":
-                break
-            if action == "1":
-                print("Create")
-            if action == "2":
-                print("List")          
+        # while True:
+        #     print("1. Create")
+        #     print("2. List")        
+        #     print("3. Exit")                
+        #     action = input("Ange:")
+        #     if action == "3":
+        #         break
+        #     if action == "1":
+        #         print("Create")
+        #     if action == "2":
+        #         print("List")          
